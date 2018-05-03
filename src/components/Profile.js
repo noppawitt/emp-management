e5efb25b2c5a863543f9414efb45b06fa5d5c1e8
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-05-04 03:57:30 +0700

    Fix profile layout

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Segment, Image, Header } from 'semantic-ui-react';
+import { Grid, Segment, Image, Header, Icon, List } from 'semantic-ui-react';
 import PageHeader from './PageHeader';
 import image from '../images/cat.jpg';
 import EditGeneralProfileModal from '../containers/modals/EditGeneralProfileModal';
@@ -40,68 +40,79 @@ import EditWorkProfileModal from '../containers/modals/EditWorkProfileModal';
 //   ownasset: 'Macbook Pro Mid 2017'
 // };
 
-const renderProfileBox = (title, EditModal, profile) => {
-  if (title === 'General') {
-    return (
-      <Segment.Group raised size="large">
-        <Segment padded inverted color="blue" loading={profile.isFetching}>
-          <Image src={image} size="small" centered bordered />
-        </Segment>
-        <Segment padded textAlign="center" loading={profile.isFetching}>
-          <Grid>
-            <Grid.Column floated="right" largeScreen={1} mobile={2}>
-              <EditModal />
-            </Grid.Column>
-          </Grid>
-          <h2>{profile.firstName} {profile.lastName} ({profile.nickName})</h2>
-          <h4>Citizen ID: {profile.citizenId}</h4>
-          <h4>Mobile No.: {profile.mobileNumber}</h4>
-          <h4>Email: {profile.email}</h4>
-          <h4>Facebook: {profile.facebookId}</h4>
-          <h4>Line ID: {profile.lineId}</h4>
-        </Segment>
-      </Segment.Group>
-    );
-  }
-  return (
-    <Segment raised padded size="large">
-      <Grid>
-        <Grid.Row>
-          <Grid.Column floated="left">
-            <Header>{title}</Header>
+// const renderProfileBox = (title, EditModal, profile) => (
+//   <Segment raised padded size="large">
+//     <Grid>
+//       <Grid.Row>
+//         <Grid.Column floated="left">
+//           <Header>{title}</Header>
+//         </Grid.Column>
+//         <Grid.Column floated="right" computer={1} mobile={2}>
+//           <EditModal />
+//         </Grid.Column>
+//       </Grid.Row>
+//       <Grid.Row>
+//         <Grid.Column computer={3} mobile={8}>
+//           {profile.map(p => (
+//             <p key={p.key}>{p.title}</p>
+//           ))}
+//         </Grid.Column>
+//         <Grid.Column computer={6} mobile={8}>
+//           {profile.map(p => (
+//             <p key={p.key}>{p.value}</p>
+//           ))}
+//         </Grid.Column>
+//       </Grid.Row>
+//     </Grid>
+//   </Segment>
+// );
+
+const renderProfileBox = (title, EditModal, icon, profile) => (
+  <Segment raised padded size="large">
+    <Grid>
+      <Grid.Column only="large screen" computer={3}>
+        <Icon name={icon} size="huge" />
+      </Grid.Column>
+      <Grid.Column computer={8} mobile={14}>
+        <Header size="large">{title}</Header>
+        <Grid columns={2}>
+          <Grid.Column>
+            <List>
+              {profile.map(p => (
+                <List.Item key={p.key}>
+                  <List.Header>
+                    {p.title}
+                  </List.Header>
+                </List.Item>
+              ))}
+            </List>
           </Grid.Column>
-          <Grid.Column floated="right" largeScreen={1} mobile={2}>
-            <EditModal />
+          <Grid.Column>
+            <List>
+              {profile.map(p => (
+                <List.Item key={p.key}>{p.value}</List.Item>
+              ))}
+            </List>
           </Grid.Column>
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column width={3}>
-            <p>Level</p>
-            <p>Department</p>
-            <p>Position</p>
-            <p>Contract</p>
-            <p>Start date</p>
-            <p>End date</p>
-            <p>Probation date</p>
-          </Grid.Column>
-          <Grid.Column width={6}>
-            <p>{profile.levelName}</p>
-            <p>{profile.departmentName}</p>
-            <p>{profile.positionName}</p>
-            <p>{profile.contractName}</p>
-            <p>{profile.startDate}</p>
-            <p>{profile.endDate}</p>
-            <p>{profile.probationDate}</p>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-  );
-};
+        </Grid>
+      </Grid.Column>
+      <Grid.Column floated="right" computer={1} mobile={2}>
+        <EditModal />
+      </Grid.Column>
+    </Grid>
+  </Segment>
+);
 
 const Profile = ({ profile }) => {
-  const renderGeneralProfileBox = renderProfileBox('General', EditGeneralProfileModal, profile);
-  const renderWorkProfileBox = renderProfileBox('Work', EditWorkProfileModal, profile);
+  const renderWorkProfileBox = renderProfileBox('Work', EditWorkProfileModal, 'suitcase', [
+    { key: 'level', title: 'Level', value: profile.levelName },
+    { key: 'department', title: 'Department', value: profile.departmentName },
+    { key: 'position', title: 'Position', value: profile.positionName },
+    { key: 'contract', title: 'Contract', value: profile.contractName },
+    { key: 'startDate', title: 'Start date', value: profile.startDate },
+    { key: 'endDate', title: 'End date', value: profile.endDate },
+    { key: 'probationDate', title: 'Probation date', value: profile.probationDate }
+  ]);
   // const renderEducationProfileBox = renderProfileBox('Education', EditGeneralProfileModal, profile);
   // const renderCertificationProfileBox = renderProfileBox('Certification', EditGeneralProfileModal, profile);
   // const renderAssetProfileBox = renderProfileBox('Asset', EditGeneralProfileModal, profile);
@@ -109,8 +120,25 @@ const Profile = ({ profile }) => {
     <div>
       <PageHeader icon="user" text="Profile" />
       <Grid centered>
-        <Grid.Column largeScreen={12} mobile={16}>
-          {renderGeneralProfileBox}
+        <Grid.Column computer={12} mobile={16}>
+          <Segment.Group raised size="large">
+            <Segment padded inverted color="blue">
+              <Image src={image} size="small" centered bordered />
+            </Segment>
+            <Segment padded textAlign="center">
+              <Grid>
+                <Grid.Column floated="right" computer={1} mobile={2}>
+                  <EditGeneralProfileModal />
+                </Grid.Column>
+              </Grid>
+              <h2>{profile.firstName} {profile.lastName} ({profile.nickName})</h2>
+              <h4>Citizen ID: {profile.citizenId}</h4>
+              <h4>Mobile No.: {profile.mobileNumber}</h4>
+              <h4>Email: {profile.email}</h4>
+              <h4>Facebook: {profile.facebookId}</h4>
+              <h4>Line ID: {profile.lineId}</h4>
+            </Segment>
+          </Segment.Group>
           {renderWorkProfileBox}
         </Grid.Column>
       </Grid>
