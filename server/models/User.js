8487768ac3dc9e09ef35c381555982b65778c8b5
Thanapon Sirisompark <tmark_s@hotmail.com>
2018-05-08 17:20:20 +0700

    Edit findAll Users

--- server/models/User.js
+++ server/models/User.js
@@ -52,7 +52,7 @@ User.findByUsername = username => (
 );
 
 User.findAll = () => (
-  db.manyOrNone('SELECT * FROM users WHERE status = $1', ['Active'])
+  db.manyOrNone('SELECT users.id, employee_info.first_name, employee_info.last_name, employee_info.nick_name, employee_info.mobile_number, employee_info.email, employee_info.picture FROM employee_info, users WHERE users.id = employee_info.user_id AND users.status = $1 ORDER BY users.id ', ['Active'])
 );
 
 module.exports = User;
