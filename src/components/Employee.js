8c038406bd3a3b53b05e50dd52a263f4b83c2c5a
5730367321-PC <j_pcr@hotmail.com>
2018-05-22 21:09:11 +0700

    loginScreen

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,62 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import { Grid, Item, Segment } from 'semantic-ui-react';
-import PageHeader from './PageHeader';
-import image from '../images/cat.jpg';
-
-const items = () => (
-  <Item>
-    <Item.Image size="small" src={image} />
-    <Item.Content >
-      <Item.Header ><br /><br />Phatchara Chokdurong [J]</Item.Header>
-      <Item.Description > Tel. 088-498-1908</Item.Description>
-      <Item.Description > Email : j_pcr@hotmail.com</Item.Description>
-    </Item.Content>
-  </Item>
-);
-const Employee = () => (
-  <div>
-    <PageHeader icon="users" text="Employee" />
-    <div>
-      <Grid columns={2} textAlign="left" >
-        <Grid.Column width={7} >
-          <Segment raised>
-            <Item.Group>
-              {items()}
-            </Item.Group>
-          </Segment>
-        </Grid.Column >
-        <Grid.Column width={7} >
-          <Segment raised>
-            <Item.Group>
-              {items()}
-            </Item.Group>
-          </Segment>
-        </Grid.Column >
-        <Grid.Column width={7} >
-          <Segment raised>
-            <Item.Group>
-              {items()}
-            </Item.Group>
-          </Segment>
-        </Grid.Column >
-        <Grid.Column width={7} >
-          <Segment raised>
-            <Item.Group>
-              {items()}
-            </Item.Group>
-          </Segment>
-        </Grid.Column >
-        <Grid.Column width={7} >
-          <Segment raised>
-            <Item.Group>
-              {items()}
-            </Item.Group>
-          </Segment>
-        </Grid.Column >
-      </Grid>
-    </div>
-=======
 import PropTypes from 'prop-types';
 import { Grid, Item, Segment, Input } from 'semantic-ui-react';
 import PageHeader from './PageHeader';
@@ -86,7 +28,6 @@ const Employee = ({ employees, onChange }) => (
     <Grid columns={2} textAlign="left">
       {employees.map(employee => items(employee))}
     </Grid>
->>>>>>> c7cbf2c00f80d12862c134f00c736aaa11b2012b
   </div>
 );
 
--- src/containers/pages/LoginPage.js
+++ src/containers/pages/LoginPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Segment, Form, Checkbox, Button } from 'semantic-ui-react';
+import { Grid, Form, Checkbox, Button, Feed, Image } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { loginRequest } from '../../actions/auth';
+import image from '../../images/Timesheet_Home.jpg';
+import TelIcon from '../../images/TelIcon.jpg';
+import MailIcon from '../../images/MailIcon.jpg';
+import GPSIcon from '../../images/GPSIcon.jpg';
 
 const LoginPage = ({ dispatch }) => {
   let username;
@@ -18,20 +22,27 @@ const LoginPage = ({ dispatch }) => {
     password.value = '';
   };
 
+  const style = {
+    backgroundImage: `url(${image})`,
+    backgroundSize: 'cover',
+    // maxWidth: '1229px',
+    minHeight: '750px'
+  };
+
   return (
-    <Grid centered columns={3} verticalAlign="middle">
-      <Grid.Row>
-        <Grid.Column>
-          <Segment>
+    <div style={style}>
+      <Grid columns={3} >
+        <Grid.Row >
+          <Grid.Column floated="right" style={{ paddingTop: '20%', paddingRight: '10%' }}>
             <Form onSubmit={handleSubmit}>
-              <Form.Field>
+              <Form.Field >
                 <label htmlFor="username">Username
-                  <input id="username" placeholder="Username" ref={(node) => { username = node; }} />
+                  <input style={{ borderRadius: '15px' }} id="username" placeholder="Username" ref={(node) => { username = node; }} />
                 </label>
               </Form.Field>
               <Form.Field>
                 <label htmlFor="password">Password
-                  <input id="password" type="password" placeholder="Password" ref={(node) => { password = node; }} />
+                  <input style={{ borderRadius: '15px' }} id="password" type="password" placeholder="Password" ref={(node) => { password = node; }} />
                 </label>
               </Form.Field>
               <Form.Field>
@@ -39,10 +50,40 @@ const LoginPage = ({ dispatch }) => {
               </Form.Field>
               <Button type="submit">Submit</Button>
             </Form>
-          </Segment>
-        </Grid.Column>
-      </Grid.Row>
-    </Grid>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row >
+          <Grid.Column floated="left" style={{ paddingLeft: '3%' }}>
+            <Feed size="large">
+              <Feed.Event>
+                <Feed.Label>
+                  <Image src={TelIcon} />
+                </Feed.Label>
+                <Feed.Content>
+                  <p style={{ color: 'white' }}><b>+66(0)2-812-7877</b></p>
+                </Feed.Content>
+              </Feed.Event>
+              <Feed.Event>
+                <Feed.Label>
+                  <Image src={MailIcon} />
+                </Feed.Label>
+                <Feed.Content>
+                  <p style={{ color: 'white' }}><b>play@playtorium.co.th</b></p>
+                </Feed.Content>
+              </Feed.Event>
+              <Feed.Event>
+                <Feed.Label>
+                  <Image src={GPSIcon} />
+                </Feed.Label>
+                <Feed.Content>
+                  <p style={{ color: 'white' }}><b>349,SJ Infinite One Business Complex,<br />11th Floor,Vibhavadi-Rangsit Road,<br />Chompol, Chatuchak, Bangkok 10900</b></p>
+                </Feed.Content>
+              </Feed.Event>
+            </Feed>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 };
 
