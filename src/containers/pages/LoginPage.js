97c4c051ef1649fad55018d80b5b2697730cf70d
5730367321-PC <j_pcr@hotmail.com>
2018-05-29 22:25:37 +0700

    fixloginscren

--- src/containers/pages/LoginPage.js
+++ src/containers/pages/LoginPage.js
@@ -25,13 +25,6 @@ const LoginPage = ({ dispatch }) => {
   const style = {
     backgroundImage: `url(${image})`,
     backgroundSize: 'cover',
-    // position: 'absolute',
-    // top: '0',
-    // left: '0',
-    // right: '0',
-    // bottom: '0',
-    // margin: 'auto',
-    // minWidth: '50%',
     minHeight: '100%',
     position: 'fixed',
     width: '100%',
