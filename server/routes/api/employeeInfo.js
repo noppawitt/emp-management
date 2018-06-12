8365a4b1ac85c97a290686767860e83244a11cac
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-06-12 15:40:00 +0700

    Add profile image component

--- server/routes/api/employeeInfo.js
+++ server/routes/api/employeeInfo.js
@@ -2,21 +2,22 @@ const router = require('express').Router();
 const EmployeeInfoController = require('../../controllers/EmployeeInfoController');
 const multer = require('multer');
 
-const storage = multer.diskStorage({
-  destination: (req, res, cb) => {
-    cb(null, 'resources/profile-img');
-  },
-  filename: (req, res, cb) => {
-    cb(null, String(req.user.id));
-  }
-});
-const upload = multer({ storage });
-
 router.post('/', EmployeeInfoController.create);
 
 router.get('/', EmployeeInfoController.find);
 
 router.put('/', EmployeeInfoController.update);
+
+const storage = multer.diskStorage({
+  destination: (req, res, cb) => {
+    cb(null, 'storage/profile-img');
+  },
+  filename: (req, res, cb) => {
+    cb(null, String(req.user.id));
+  }
+});
+
+const upload = multer({ storage });
 
 router.post('/upload-profile-img', upload.single('profile'), EmployeeInfoController.updateProfileImg);
 
