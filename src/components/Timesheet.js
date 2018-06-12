9834297dad68bb02d048cbd6787cad96307ff7f1
5730367321-PC <j_pcr@hotmail.com>
2018-06-12 09:59:39 +0700

    calendar

--- src/components/Timesheet.js
+++ src/components/Timesheet.js
@@ -4,8 +4,8 @@ import { Icon, Table, Label, Grid, Segment } from 'semantic-ui-react';
 class Timesheet extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { 
-      month: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    this.state = {
+      month: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       dayOfMonth: [31,28,31,30,31,30,31,31,30,31,30,31],
       days: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
     };
@@ -18,9 +18,12 @@ class Timesheet extends React.Component {
     };
     return (
       <div>
-        <Table celled>
+        <Table celled structured>
           <Table.Header>
-            <Table.Row>
+            <Table.Row textAlign="center">
+              <Table.HeaderCell colSpan="7"><font size="3">{this.state.month[6]}</font> 2018</Table.HeaderCell>
+            </Table.Row>
+            <Table.Row textAlign="center">
               {this.state.days.map(day => <Table.HeaderCell>{day}</Table.HeaderCell>)}
             </Table.Row>
           </Table.Header>
@@ -28,8 +31,8 @@ class Timesheet extends React.Component {
             <Table.Row style={{ height: '10em', maxWidth: '10em' }} >
               <Table.Cell style={{ backgroundColor: colorSunSat }} >
                 <Grid.Column>
-                  <Grid.Row textAlign="right">
-                    1
+                  <Grid.Row textAlign="right" >
+                    <font color="red" size="3">31</font>
                   </Grid.Row>
                   <Grid.Row style={{ height: '5em' }} />
                   <Grid.Row textAlign="center">
@@ -42,7 +45,20 @@ class Timesheet extends React.Component {
               <Table.Cell>
                 <Grid.Column>
                   <Grid.Row textAlign="right">
-                    1
+                    <font color="blue">1</font>
+                  </Grid.Row>
+                  <Grid.Row style={{ height: '5em' }} />
+                  <Grid.Row textAlign="center">
+                    <Label as="a" basic color="red" size="medium" >
+                      <Icon name="pencil alternate" /><font size="2">7  Hour</font>
+                    </Label>
+                  </Grid.Row>
+                </Grid.Column>
+              </Table.Cell>
+              <Table.Cell>
+                <Grid.Column>
+                  <Grid.Row textAlign="right">
+                    <font color="blue">1</font>
                   </Grid.Row>
                   <Grid.Row style={{ height: '5em' }} />
                   <Grid.Row textAlign="center">
@@ -55,7 +71,20 @@ class Timesheet extends React.Component {
               <Table.Cell>
                 <Grid.Column>
                   <Grid.Row textAlign="right">
-                    1
+                    <font color="blue">1</font>
+                  </Grid.Row>
+                  <Grid.Row style={{ height: '5em' }} />
+                  <Grid.Row textAlign="center">
+                    <Label as="a" basic color="green" size="medium" >
+                      <Icon name="pencil alternate" /><font size="2">วิสาฆบูชา</font>
+                    </Label>
+                  </Grid.Row>
+                </Grid.Column>
+              </Table.Cell>
+              <Table.Cell>
+                <Grid.Column>
+                  <Grid.Row textAlign="right">
+                    <font color="blue">1</font>
                   </Grid.Row>
                   <Grid.Row style={{ height: '5em' }} />
                   <Grid.Row textAlign="center">
@@ -68,33 +97,7 @@ class Timesheet extends React.Component {
               <Table.Cell>
                 <Grid.Column>
                   <Grid.Row textAlign="right">
-                    1
-                  </Grid.Row>
-                  <Grid.Row style={{ height: '5em' }} />
-                  <Grid.Row textAlign="center">
-                    <Label as="a" basic color="blue" size="medium" >
-                      <Icon name="pencil alternate" /><font size="2">8  Hour</font>
-                    </Label>
-                  </Grid.Row>
-                </Grid.Column>
-              </Table.Cell>
-              <Table.Cell>
-                <Grid.Column>
-                  <Grid.Row textAlign="right">
-                    1
-                  </Grid.Row>
-                  <Grid.Row style={{ height: '5em' }} />
-                  <Grid.Row textAlign="center">
-                    <Label as="a" basic color="blue" size="medium" >
-                      <Icon name="pencil alternate" /><font size="2">8  Hour</font>
-                    </Label>
-                  </Grid.Row>
-                </Grid.Column>
-              </Table.Cell>
-              <Table.Cell>
-                <Grid.Column>
-                  <Grid.Row textAlign="right">
-                    1
+                    <font color="blue">1</font>
                   </Grid.Row>
                   <Grid.Row style={{ height: '5em' }} />
                   <Grid.Row textAlign="center">
@@ -107,7 +110,7 @@ class Timesheet extends React.Component {
               <Table.Cell style={{ backgroundColor: colorSunSat }}>
                 <Grid.Column>
                   <Grid.Row textAlign="right">
-                    1
+                    <font color="blue">1</font>
                   </Grid.Row>
                   <Grid.Row style={{ height: '5em' }} />
                   <Grid.Row textAlign="center">
