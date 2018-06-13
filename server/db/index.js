af6261c8b2c63acd4e1366d2f20470d03fe069f3
NothingSOS <bestnothing@hotmail.com>
2018-06-13 17:58:00 +0900

    update ssl for connecting DB

--- server/db/index.js
+++ server/db/index.js
@@ -31,7 +31,8 @@ const config = {
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  ssl: true
 };
 
 const db = pgp(config);
