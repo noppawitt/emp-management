72b7dccf061332757e6ea769fa8e53748c75545f
NothingSOS <bestnothing@hotmail.com>
2018-06-14 19:54:36 +0900

    Fix ExamPage structure and try modal (still use employee modal)

---
+++ src/components/Exam.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Segment, Button, Icon, Grid } from 'semantic-ui-react';
+import { Switch, Route, Link } from 'react-router-dom';
+
+const HeadExam = (({ subject }) => {
+  if (subject === 'english') {
+    return [
+      <Link to="/exam/english/"><Button primary>ENGLISH</Button></Link>,
+      <Link to="/exam/logic/"><Button secondary>LOGIC</Button></Link>,
+      <Link to="/exam/skill/"><Button secondary>SKILL</Button></Link>
+    ];
+  }
+  else if (subject === 'logic') {
+    return [
+      <Link to="/exam/english/"><Button secondary>ENGLISH</Button></Link>,
+      <Link to="/exam/logic/"><Button primary>LOGIC</Button></Link>,
+      <Link to="/exam/skill/"><Button secondary>SKILL</Button></Link>
+    ];
+  }
+  return [
+    <Link to="/exam/english/"><Button secondary>ENGLISH</Button></Link>,
+    <Link to="/exam/logic/"><Button secondary>LOGIC</Button></Link>,
+    <Link to="/exam/skill/"><Button primary>SKILL</Button></Link>
+  ];
+});
+HeadExam.propTypes = {
+  subject: PropTypes.string.isRequired
+};
+
+const Exam = (({ onClick }) => (
+  <div>
+    <Segment.Group>
+      <Segment>
+        <Grid>
+          <Grid.Column width={6}>
+            <Switch>
+              <Route exact path="/exam/english/" render={() => <HeadExam subject="english" />} />
+              <Route exact path="/exam/logic/" render={() => <HeadExam subject="logic" />} />
+              <Route exact path="/exam/skill/" render={() => <HeadExam subject="skill" />} />
+              <Route render={() => <HeadExam subject="english" />} />
+            </Switch>
+          </Grid.Column>
+          <Grid.Column width={10}>
+            <Button icon labelPosition="left" floated="right" color="green" onClick={onClick}>
+              <Icon name="add" />
+              Add new question
+            </Button>
+          </Grid.Column>
+        </Grid>
+      </Segment>
+    </Segment.Group>
+  </div>
+));
+
+Exam.propTypes = {
+  onClick: PropTypes.func.isRequired
+};
+
+export default Exam;
--- src/containers/pages/ExamPage.js
+++ src/containers/pages/ExamPage.js
@@ -1,60 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Segment, Button, Icon, Grid } from 'semantic-ui-react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { compose, lifecycle } from 'recompose';
+import Exam from '../../components/Exam';
+import { openModal } from '../../actions/modal';
+import * as modalNames from '../../constants/modalNames';
+import Loader from '../../components/Loader';
+import { fetchEmployeeRequest } from '../../actions/employee';
 
-const HeadExam = (({ subject }) => {
-  if (subject === 'english') {
-    return (
-      <Grid.Column width={6}>
-        <Link to="/exam/english/"><Button primary>ENGLISH</Button></Link>
-        <Link to="/exam/logic/"><Button secondary>LOGIC</Button></Link>
-        <Link to="/exam/skill/"><Button secondary>SKILL</Button></Link>
-      </Grid.Column>
-    );
-  }
-  else if (subject === 'logic') {
-    return (
-      <Grid.Column width={6}>
-        <Link to="/exam/english/"><Button secondary>ENGLISH</Button></Link>
-        <Link to="/exam/logic/"><Button primary>LOGIC</Button></Link>
-        <Link to="/exam/skill/"><Button secondary>SKILL</Button></Link>
-      </Grid.Column>
-    );
-  }
-  return (
-    <Grid.Column width={6}>
-      <Link to="/exam/english/"><Button secondary>ENGLISH</Button></Link>
-      <Link to="/exam/logic/"><Button secondary>LOGIC</Button></Link>
-      <Link to="/exam/skill/"><Button primary>SKILL</Button></Link>
-    </Grid.Column>
-  );
-});
-HeadExam.propTypes = {
-  subject: PropTypes.string.isRequired
+const ExamPage = ({ isFetching, onClick }) => (
+  <div>
+    {isFetching ? <Loader /> : <Exam onClick={onClick} />}
+  </div>
+);
+
+ExamPage.propTypes = {
+  isFetching: PropTypes.bool.isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
-const ExamPage = (() => (
-  <div>
-    <Segment.Group>
-      <Segment>
-        <Grid>
-          <Switch>
-            <Route exact path="/exam/english/" render={() => <HeadExam subject="english" />} />
-            <Route exact path="/exam/logic/" render={() => <HeadExam subject="logic" />} />
-            <Route exact path="/exam/skill/" render={() => <HeadExam subject="skill" />} />
-            <Route render={() => <HeadExam subject="english" />} />
-          </Switch>
-          <Grid.Column width={10}>
-            <Button icon labelPosition="left" floated="right" color="green">
-              <Icon name="add" />
-              Add new question
-            </Button>
-          </Grid.Column>
-        </Grid>
-      </Segment>
-    </Segment.Group>
-  </div>
-));
+const mapStateToProps = state => ({
+  isFetching: state.employee.isFetching
+});
+const mapDispatchToProps = dispatch => ({
+  fetchEmployee: () => dispatch(fetchEmployeeRequest()),
+  onClick: () => dispatch(openModal(modalNames.ADD_EMPLOYEE))
+});
+const enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  lifecycle({
+    componentDidMount() {
+      const { fetchEmployee } = this.props;
+      fetchEmployee();
+    }
+  })
+);
+export default enhance(ExamPage);
 
-export default ExamPage;
