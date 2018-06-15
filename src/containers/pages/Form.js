464bde34dd3ae7cbf890e1a8d0831f7c5c385dec
Wachit Chaisitsak <cyh@Wachits-MacBook-Pro.local>
2018-06-15 14:50:50 +0700

    font-weight change

--- src/containers/pages/Form.js
+++ src/containers/pages/Form.js
@@ -98,19 +98,19 @@ class Form extends Component {
     this.setState({mainOption: x});
   }
   handle_accept1() {
-    document.getElementById('emp').innerHTML = 'Date: ' + (new Date()).toString().substr(0, 24);
+    document.getElementById('emp').innerHTML = (new Date()).toString().substr(0, 24);
     document.getElementById('mark1').style.width = '2em';
     document.getElementById('sup-but').disabled = false;
     document.getElementById('emp-but').disabled = true;
   }
   handle_accept2() {
-    document.getElementById('sup').innerHTML = 'Date: ' + (new Date()).toString().substr(0, 24);
+    document.getElementById('sup').innerHTML = (new Date()).toString().substr(0, 24);
     document.getElementById('mark2').style.width = '2em';
     document.getElementById('mag-but').disabled = false;
     document.getElementById('sup-but').disabled = true;
   }
   handle_accept3() {
-    document.getElementById('mag').innerHTML = 'Date: ' + (new Date()).toString().substr(0, 24);
+    document.getElementById('mag').innerHTML = (new Date()).toString().substr(0, 24);
     document.getElementById('mark3').style.width = '2em';
     document.getElementById('mag-but').disabled = true;
   }
@@ -292,9 +292,9 @@ class Form extends Component {
                       <th><div className='div_mark' ><button className="Accept-button" id='mag-but' onClick={this.handle_accept3}>Accept</button><div className="div_mark3" id="mark3"><img src={cmark} className="mark"></img></div></div></th>
                     </tr>
                     <tr>
-                      <th id="emp">Date:</th>
-                      <th id="sup">Date:</th>
-                      <th id="mag">Date:</th>
+                      <th>Date: <span id="emp" className="date"></span></th>
+                      <th>Date: <span id="sup" className="date"></span></th>
+                      <th>Date: <span id="mag" className="date"></span></th>
                     </tr>
                   </tbody>
                 </table>
