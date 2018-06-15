cbe67b398dfdbd58b3308631ed63250e1674b155
Magic <homesnet1234@hotmail.com>
2018-06-15 09:53:48 +0700

    Updated Probation Query code

--- server/models/Probation.js
+++ server/models/Probation.js
@@ -3,9 +3,12 @@ const db = require('../db/');
 const Probation = {};
 
 Probation.findById = (id) => (
-    db.manyOrNone('SELECT first_name, department_id, i.user_id, position_id, level_id, start_date, probation_date '
-    + 'FROM public.employee_work w, public.employee_info i WHERE i.user_id = $1' +
-    ' ORDER BY i.user_id ASC', [id])
+    db.manyOrNone("SELECT CONCAT(i.first_name, ' ', i.last_name) as full_name, d.name as department, i.user_id, po.name as position, CONCAT(w.level_id, '. ', l.name) as level"
+    + ", w.start_date, w.probation_date, bo.boss_name as supervisor" +
+    " FROM public.employee_work w, public.employee_info i, public.departments d, public.positions po, public.levels l, " +
+    "(SELECT i.user_id ,CONCAT(i.first_name, ' ', i.last_name) as boss_name FROM public.employee_info i, public.employee_work w WHERE i.user_id = w.boss_id) bo " +
+    "WHERE i.user_id = $1 AND w.position_id = po.id AND w.department_id = d.id AND bo.user_id = w.boss_id AND w.level_id = l.id " +
+    "ORDER BY i.user_id ASC", [id])
 )
 
 module.exports = Probation;
