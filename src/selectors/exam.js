e9542bea21ac46785a22a2cce79a965fa60220cb
NothingSOS <bestnothing@hotmail.com>
2018-06-22 13:23:38 +0900

    Fix search function in ExamPage

--- src/selectors/exam.js
+++ src/selectors/exam.js
@@ -2,5 +2,5 @@ export const getVisibleExams = (state) => {
   if (!state.exam.lists) return [];
 
   return state.exam.lists.filter(exam => (state.exam.searchText === undefined || state.exam.searchText === '' || exam.exQuestion.includes(state.exam.searchText))
-  && (state.exam.searchCategory === undefined || state.exam.searchCategory === 'all subjects' || state.exam.searchCategory === exam.exSubcategory));
+    && (state.exam.searchCategory === undefined || state.exam.searchCategory === 'all subjects' || state.exam.searchCategory === exam.exCategory));
 };
