2701a62b7569c25ee8a862bc4bba81398335fd5e
Magic <homesnet1234@hotmail.com>
2018-06-22 10:51:25 +0700

    Change SignatureButton to React style

--- src/containers/pages/components/SignatureComponent.js
+++ src/containers/pages/components/SignatureComponent.js
@@ -3,22 +3,45 @@ import cmark from '../pic/mark.png';
 import './css/SignatureComponent.css';
 
 class SignatureComponent extends React.Component {
-    handle_accept1() {
-        document.getElementById('emp').innerHTML = (new Date()).toString().substr(0, 24);
+    constructor(props) {
+        super(props);
+        this.state = {
+            employeeSignDate: '',
+            supervisorSignDate: '',
+            MDSignDate: ''
+        };
+
+        this.EmployeeSignHandler = this.EmployeeSignHandler.bind(this);
+        this.SupervisorSignHandler = this.SupervisorSignHandler.bind(this);
+        this.MDSignHandler = this.MDSignHandler.bind(this);
+
+        this.employeeSignButton = React.createRef();
+        this.supervisorSignButton = React.createRef();
+        this.MDSignButton = React.createRef();
+    }
+
+    componentDidMount() {
+        this.supervisorSignButton.current.disabled = true;
+        this.MDSignButton.current.disabled = true;
+    }
+
+    EmployeeSignHandler() {
+        this.setState({ employeeSignDate: new Date().toString().substr(0, 24) });
         document.getElementById('mark1').style.width = '2em';
-        document.getElementById('sup-but').disabled = false;
-        document.getElementById('emp-but').disabled = true;
+        this.employeeSignButton.current.disabled = true;
+        this.supervisorSignButton.current.disabled = false;
+        this.MDSignButton.current.disabled = true;
     }
-    handle_accept2() {
-        document.getElementById('sup').innerHTML = (new Date()).toString().substr(0, 24);
+    SupervisorSignHandler() {
+        this.setState({ supervisorSignDate: new Date().toString().substr(0, 24) });
         document.getElementById('mark2').style.width = '2em';
-        document.getElementById('mag-but').disabled = false;
-        document.getElementById('sup-but').disabled = true;
+        this.supervisorSignButton.current.disabled = true;
+        this.MDSignButton.current.disabled = false;
     }
-    handle_accept3() {
-        document.getElementById('mag').innerHTML = (new Date()).toString().substr(0, 24);
+    MDSignHandler() {
+        this.setState({ MDSignDate: new Date().toString().substr(0, 24) });
         document.getElementById('mark3').style.width = '2em';
-        document.getElementById('mag-but').disabled = true;
+        this.MDSignButton.current.disabled = true;
     }
 
     render() {
@@ -36,14 +59,29 @@ class SignatureComponent extends React.Component {
                                 <th>Managing Director signature:</th>
                             </tr>
                             <tr>
-                                <th><div className='div_mark' ><button className="Accept-button" id='emp-but' onClick={this.handle_accept1}>Accept</button><div className="div_mark1" id="mark1"><img src={cmark} className="mark"></img></div></div></th>
-                                <th><div className='div_mark' ><button className="Accept-button" id='sup-but' onClick={this.handle_accept2}>Accept</button><div className="div_mark2" id="mark2"><img src={cmark} className="mark"></img></div></div></th>
-                                <th><div className='div_mark' ><button className="Accept-button" id='mag-but' onClick={this.handle_accept3}>Accept</button><div className="div_mark3" id="mark3"><img src={cmark} className="mark"></img></div></div></th>
+                                <th>
+                                    <div className='div_mark'>
+                                        <button ref={this.employeeSignButton} className="Accept-button" onClick={this.EmployeeSignHandler}>Accept</button>
+                                        <div className="div_mark1" id="mark1"><img src={cmark} className="mark"></img></div>
+                                    </div>
+                                </th>
+                                <th>
+                                    <div className='div_mark'>
+                                        <button ref={this.supervisorSignButton} className="Accept-button" onClick={this.SupervisorSignHandler}>Accept</button>
+                                        <div className="div_mark2" id="mark2"><img src={cmark} className="mark"></img></div>
+                                    </div>
+                                </th>
+                                <th>
+                                    <div className='div_mark'>
+                                        <button ref={this.MDSignButton} className="Accept-button" onClick={this.MDSignHandler}>Accept</button>
+                                        <div className="div_mark3" id="mark3"><img src={cmark} className="mark"></img></div>
+                                    </div>
+                                </th>
                             </tr>
                             <tr>
-                                <th>Date: <span id="emp" className="date"></span></th>
-                                <th>Date: <span id="sup" className="date"></span></th>
-                                <th>Date: <span id="mag" className="date"></span></th>
+                                <th>Date: <span className="date">{this.state.employeeSignDate}</span></th>
+                                <th>Date: <span className="date">{this.state.supervisorSignDate}</span></th>
+                                <th>Date: <span className="date">{this.state.MDSignDate}</span></th>
                             </tr>
                         </tbody>
                     </table>
