37b08253b4fc38cd0040914e70b222b8bd747a5f
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-07-02 14:16:11 +0700

    Set default probation date

--- src/containers/forms/AddProjectForm.js
+++ src/containers/forms/AddProjectForm.js
@@ -49,7 +49,7 @@ const AddProjectForm = ({ handleSubmit, submitting, setWorkingDay, paymentType }
       label="Payment type"
       placeholder="Payment type"
       options={paymentTypeOptions}
-      onChange={(event, newValue) => setWorkingDay(newValue === 'Man-month' ? 22 : null)}
+      onChange={(e, newValue) => setWorkingDay(newValue === 'Man-month' ? 22 : null)}
       disabled={submitting}
     />
     {paymentType === 'Man-month' && <Field name="workingDay" as={Form.Select} component={Input} label="Working day" placeholder="Working day" options={workingDayOptions} disabled={submitting} />}
