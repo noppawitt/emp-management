0011356bd0af2aefafaa0e9765aac0a38de0915a
Magic <homesnet1234@hotmail.com>
2018-07-02 14:57:25 +0700

    Fixed Bug about sign order

--- src/containers/pages/components/SignatureComponent.js
+++ src/containers/pages/components/SignatureComponent.js
@@ -40,16 +40,15 @@ class SignatureComponent extends React.Component {
             this.MDSignButton.current.disabled = true;
         }
 
-        console.log("test1");
-        console.log(this.props);
-        console.log(this.state);
-
         if (this.state.employeeSignDate)
             this.EmployeeSignHandler();
+        else
+            this.supervisorSignButton.current.disabled = true;
 
         if (this.state.supervisorSignDate)
             this.SupervisorSignHandler();
-
+        else
+            this.MDSignButton.current.disabled = true;
 
         if (this.state.MDSignDate)
             this.MDSignHandler();
