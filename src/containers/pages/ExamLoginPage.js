b2d93770670b2fa8b580e19fe86bc77aae58add4
NothingSOS <bestnothing@hotmail.com>
2018-07-06 16:06:54 +0900

    Arrange style in TakeExam login page

--- src/containers/pages/ExamLoginPage.js
+++ src/containers/pages/ExamLoginPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Grid, Segment, Form, Button } from 'semantic-ui-react';
+import { Grid, Segment, Form, Button, Container, Header, Icon } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { examLoginRequest } from '../../actions/examAuth';
 
@@ -19,38 +19,52 @@ const ExamLoginPage = ({ dispatch }) => {
   };
 
   return (
-    <div>
-      {console.log('exam login page')}
-      <Grid.Column>
-        <Segment four wide center style={{ width: '300px' }}>
-          <Form onSubmit={handleSubmit}>
-            <Form.Field>
-              <br />
-              <label htmlFor="username">
-                <input
-                  id="username"
-                  placeholder="Username"
-                  style={{ borderRadius: '15px' }}
-                  ref={(node) => { id = node; }}
-                />
-              </label>
-            </Form.Field>
-            <Form.Field>
-              <label htmlFor="password">
-                <input
-                  id="password"
-                  type="password"
-                  placeholder="password"
-                  style={{ borderRadius: '15px' }}
-                  ref={(node) => { password = node; }}
-                />
-              </label>
-              <br />
-              <Button primary type="submit">Submit</Button>
-            </Form.Field>
-          </Form>
-        </Segment>
-      </Grid.Column>
+    <div style={{ backgroundColor: '#5c97b5' }}>
+      <Container textAlign="center">
+        {console.log('exam login page')}
+        <Grid.Row centered columns={2}>
+          <br />
+          <br />
+          <br />
+          <br />
+          <Grid.Column>
+            <Icon circular inverted color='teal' name="book" size="massive" />
+            <Header as="h1">Take Exam</Header>
+          </Grid.Column>
+          <br />
+          <br />
+          <Grid.Column>
+            <Segment style={{ width: '30%', margin: 'auto' }} raised>
+              <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                  <br />
+                  <label htmlFor="username">
+                    <input
+                      id="username"
+                      placeholder="Username"
+                      style={{ borderRadius: '15px' }}
+                      ref={(node) => { id = node; }}
+                    />
+                  </label>
+                </Form.Field>
+                <Form.Field>
+                  <label htmlFor="password">
+                    <input
+                      id="password"
+                      type="password"
+                      placeholder="password"
+                      style={{ borderRadius: '15px' }}
+                      ref={(node) => { password = node; }}
+                    />
+                  </label>
+                  <br />
+                  <Button primary type="submit">Submit</Button>
+                </Form.Field>
+              </Form>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+      </Container>
     </div>
   );
 };
