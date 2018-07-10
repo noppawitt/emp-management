f9d705be99634914bf38e0dd439a31d1f7d44daf
NothingSOS <bestnothing@hotmail.com>
2018-07-10 17:21:53 +0900

    Add choice number and make word-wrap

--- src/components/TakeExam.js
+++ src/components/TakeExam.js
@@ -120,12 +120,12 @@ const TakeExam = ({
                         <br />
                         {questionRenderer(row.exQuestion, row.exSubcategory)}
                         <br />
-                        {row.exType === 'Choices' && row.exChoice.map(answer => (
+                        {row.exType === 'Choices' && row.exChoice.map((answer, i) => (
                           row.exAnswerLength === 1 ?
                             <Form.Field>
                               <p>
                                 <Radio
-                                  label={answer}
+                                  label={(i + 1).toString().concat('. ').concat(answer)}
                                   value={answer}
                                   checked={showAnswerCheckRadio(row.exId, answerList, answer)}
                                   onClick={(e, { value }) => {
@@ -137,7 +137,7 @@ const TakeExam = ({
                             <Form.Field>
                               <p>
                                 <Checkbox
-                                  label={answer}
+                                  label={(i + 1).toString().concat('. ').concat(answer)}
                                   value={answer}
                                   checked={showAnswerCheckRadio(row.exId, answerList, answer)}
                                   onClick={(e, { value }) => {
