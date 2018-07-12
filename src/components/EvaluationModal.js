bed1a353291a7ca5ddb72d95ca0af4a304cb6e40
Wachit Chaisitsak <cyh@Wachits-MacBook-Pro.local>
2018-07-12 16:05:11 +0700

    close button change

--- src/components/EvaluationModal.js
+++ src/components/EvaluationModal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Modal, Button, Icon} from 'semantic-ui-react';
 import ConfirmModal from './ConfirmModal';
+import './css/ConfirmModal.css'
 
 const ModalBasicExample = ({open}) => (
   <Modal trigger={<Button>Basic Modal</Button>} basic size='small'>
