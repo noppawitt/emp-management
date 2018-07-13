d9617497183ad0a30619c3d1ade750a43082423a
nihcet <techinkid@gmail.com>
2018-07-13 11:28:19 +0700

    can't agree after login

--- server/controllers/TakeExamAgreementController.js
+++ server/controllers/TakeExamAgreementController.js
@@ -4,18 +4,19 @@ const TakeExamAgreement = require('../models/TakeExamAgreement');
 const jwtSecret = process.env.JWT_SECRET;
 
 exports.acceptAgreement = (req, res, next) => {
+  console.log('HERE', req.body);
   TakeExamAgreement.acceptAgreement(req.body.id, req.body.testdate)
-    .then((value) => {
+    .then(() => {
       const newToken = jwt.sign({
         id: req.body.id,
         agreementStatus: 'Read',
-        testdate: req.body.testDate,
+        testdate: req.body.testdate,
       }, jwtSecret);
       res.json({
         id: req.body.id,
         newToken,
         agreementStatus: 'Read',
-        testdate: req.body.testDate,
+        testdate: req.body.testdate,
       });
     })
     .catch(next);
