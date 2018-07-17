1878cf56ffa34daf1d636877e7cabd61a6d8d1bf
Noppawit Thairungroj <noppawit.trr@gmail.com>
2018-07-17 17:54:12 +0700

    Init holiday components

---
+++ src/components/Holiday.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Holiday = () => (
+  <div>
+    Holiday
+  </div>
+);
+
+export default Holiday;
---
+++ src/containers/pages/HolidayPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Holiday from '../../components/Holiday';
+
+const HolidayPage = () => (
+  <Holiday />
+);
+
+export default HolidayPage;
